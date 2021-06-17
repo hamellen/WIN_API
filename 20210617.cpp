9373027edac6e4f84fbf2dc0cad720d300312281
hamellen <78523919+hamellen@users.noreply.github.com>
2021-06-17 22:18:32 +0900

    Add files via upload

---
+++ 20210617.cpp
@@ -0,0 +1,195 @@
+﻿// 20210617.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "20210617.h"
+
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];              // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];           // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);//명령 처리 함수
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,//메인함수
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY20210617, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY20210617));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:,메세지 처리
+    while (GetMessage(&msg, nullptr, 0, 0))//메세지 가져오기
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);//메세지 번역
+            DispatchMessage(&msg);//메세지를 원도우 프로시저로 보내기
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)//윈도우 클래스 등록
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;//가로세로 길이 변경
+    wcex.lpfnWndProc    = WndProc;//메세지 처리할 프로시저 이름 설정
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, IDI_APPLICATION);//원도우 아이콘 설정
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);//커서 설정
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName = MAKEINTRESOURCE(IDC_MY20210617);//메뉴 설정
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, IDI_APPLICATION);//원도우 아이콘 설정
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,//윈도우 생성
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);//원도우 화면에 출력
+   UpdateWindow(hWnd);//클라이언트 영역 갱신
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)//메세지를 처리하는 핵심 함수
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT://윈도우 클라이언트 영역에서 일어나는 모든일 총괄 함수,hdc 꼭 포홤시키기 
+        {
+            PAINTSTRUCT ps;//그리기 정보를 가지고 있는 구조체
+            HDC hdc = BeginPaint(hWnd, &ps);//DC 얻기 함수
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            SetTextColor(hdc, RGB(150, 0, 200));//문자색 설정
+            SetBkColor(hdc, RGB(0, 0, 255));//문자 배경색 설정
+            //TextOut(hdc, 200, 200, TEXT("HAMELLEN"), 8);//TEXT의 문자수와 길이가 맞지 않으면 여부 문자 출력,배치 관리자 미적용 
+            //SetTextAlign(hdc,TA_BOTTOM | TA_RIGHT); 다음문자정렬 위치 설정
+            RECT rt;//RECT 구조체 변수 저장
+            //GetClientRect(hWnd, &rt);//윈도우의 클라이언트 영역의 사각형 정보 rt에 저장
+            //DrawTextW(hdc, TEXT("안녕"), -1, &rt, DT_CENTER);//배치관리자 적용
+            //for (int x = 100; x <= 300; x++)
+                //for (int y = 100; y <= 400; y++)
+                    //SetPixel(hdc, x, y, RGB(150, 0, 200));픽셀 연습본,비효율적
+            //LineTo(hdc,100,100) 디폴트 죄표(0,0)에서 (100,100)까지 직선을 귿는다. 변경된 좌표는 저장된다. 
+            //MoveToEx(hdc,200,200,NULL) 변경된 좌표에서 (200,200)으로 좌표이동 
+            //Rectangle(hdc,100,300,300,500); 직사각형 출력
+            //Ellipse(hdc,100,200,200,400); 타원 출력
+            EndPaint(hWnd, &ps);//DC 반환 함수
+        }
+        break;
+    case WM_DESTROY://원도우 종료 메세지
+        PostQuitMessage(0);
+        break;
+    default://case에 의해 처리되지 않은 메세지 처리 
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)//대화상자에서 발생하는 메세지 처리
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
