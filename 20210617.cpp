220b17dfdb13233facbb7ae63cf33ed10cba4c1d
hamellen <78523919+hamellen@users.noreply.github.com>
2021-06-20 19:42:02 +0900

    Add files via upload

--- 20210617.cpp
+++ 20210617.cpp
@@ -69,7 +69,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)//윈도우 클래스 등록
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;//가로세로 길이 변경
+    wcex.style          = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;//가로세로 길이 변경,마우스 더블클릭 여부
     wcex.lpfnWndProc    = WndProc;//메세지 처리할 프로시저 이름 설정
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
@@ -122,10 +122,69 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
 //
 //
+#define maxstring 128
+#define r 50
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)//메세지를 처리하는 핵심 함수
 {
+    static HPEN hBluePen;//펜 생성
+    static POINT ptStart, ptEnd;//직선 시작지점,끝나는 지점 좌표
+    static POINT ptMouse;//마우스 위지정보
+    static  BOOL bDraw = FALSE;//마우스 누름정보 
+    static WCHAR str[maxstring + 1];
+    static WCHAR str2[maxstring + 1];
+    int n;
     switch (message)
     {
+    case WM_CREATE:
+        hBluePen = CreatePen(PS_DASH, 1, RGB(0, 0, 255));//펜생성
+        break;
+    case WM_LBUTTONDOWN:
+        ptStart.x = LOWORD(lParam);//마우스  x 위치
+        ptStart.y = HIWORD(lParam);//마우스 y 위치
+        ptEnd = ptStart;
+        break;
+    case WM_LBUTTONUP://마우스왼쪽의 누름이 사라지는 순간 출력설정
+        bDraw = TRUE;
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+    case WM_MOUSEMOVE:
+        if (wParam && MK_LBUTTON)//마우스 왼쪽 관련 
+        {
+            HDC hdc = GetDC(hWnd);
+            int OldMode = SetROP2(hdc, R2_NOTXORPEN);//레스터연산모드
+
+            HPEN hOldPen = (HPEN)SelectObject(hdc, hBluePen);
+            MoveToEx(hdc, ptStart.x, ptStart.y, NULL);//(0,0)에서 직선시작지점으로 좌표이동
+            LineTo(hdc, ptEnd.x, ptEnd.y);//처음좌표에서 마지막좌표까지의 직선출력
+
+            ptEnd.x = LOWORD(lParam);//마지막 x좌표
+            ptEnd.y = HIWORD(lParam);//마지막 y좌표
+            MoveToEx(hdc, ptStart.x, ptStart.y, NULL);//(0,0)에서 직선시작지점으로 좌표이동
+            LineTo(hdc, ptEnd.x, ptEnd.y);//처음좌표에서 마지막좌표까지의 직선출력
+
+            SelectObject(hdc, hOldPen);
+            SetROP2(hdc, OldMode);
+            ReleaseDC(hWnd, hdc);
+        }
+        break;
+    case WM_KEYDOWN://키다운 호출
+        n = lstrlen(str);
+        if (n < maxstring&&IsCharAlphaNumeric(wParam))
+            str[n] = (WCHAR)wParam;
+        if (n > 0 && wParam == 8)
+            str[n - 1] = 0;
+
+        //InvalidateRect(hWnd, NULL, TRUE);//타원 이동흔적 제거.FALSE면 타원의 이동흔적이 남는다. 
+        break;
+    case WM_CHAR://문자 호출
+        n = lstrlen(str2);
+        if (wParam == 8 && n > 0)
+            str2[n - 1] = 0;
+        else
+            str2[n] = (WCHAR)wParam;
+        
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -148,24 +207,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)/
             PAINTSTRUCT ps;//그리기 정보를 가지고 있는 구조체
             HDC hdc = BeginPaint(hWnd, &ps);//DC 얻기 함수
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            SetTextColor(hdc, RGB(150, 0, 200));//문자색 설정
-            SetBkColor(hdc, RGB(0, 0, 255));//문자 배경색 설정
-            //TextOut(hdc, 200, 200, TEXT("HAMELLEN"), 8);//TEXT의 문자수와 길이가 맞지 않으면 여부 문자 출력,배치 관리자 미적용 
-            //SetTextAlign(hdc,TA_BOTTOM | TA_RIGHT); 다음문자정렬 위치 설정
-            RECT rt;//RECT 구조체 변수 저장
-            //GetClientRect(hWnd, &rt);//윈도우의 클라이언트 영역의 사각형 정보 rt에 저장
-            //DrawTextW(hdc, TEXT("안녕"), -1, &rt, DT_CENTER);//배치관리자 적용
-            //for (int x = 100; x <= 300; x++)
-                //for (int y = 100; y <= 400; y++)
-                    //SetPixel(hdc, x, y, RGB(150, 0, 200));픽셀 연습본,비효율적
-            //LineTo(hdc,100,100) 디폴트 죄표(0,0)에서 (100,100)까지 직선을 귿는다. 변경된 좌표는 저장된다. 
-            //MoveToEx(hdc,200,200,NULL) 변경된 좌표에서 (200,200)으로 좌표이동 
-            //Rectangle(hdc,100,300,300,500); 직사각형 출력
-            //Ellipse(hdc,100,200,200,400); 타원 출력
+            if (bDraw)//영역무력화 호출시 발동 
+            {
+                MoveToEx(hdc, ptStart.x, ptStart.y, NULL);//초기좌표
+                LineTo(hdc, ptEnd.x, ptEnd.y);//마지막 좌표까지의 선긋기
+
+
+            }
+                
             EndPaint(hWnd, &ps);//DC 반환 함수
         }
         break;
     case WM_DESTROY://원도우 종료 메세지
+        DeleteObject(hBluePen);//펜오브젝트삭제
         PostQuitMessage(0);
         break;
     default://case에 의해 처리되지 않은 메세지 처리 
